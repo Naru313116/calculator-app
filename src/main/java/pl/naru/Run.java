c6ee544f1b87791993f876a6c81f551a9e28a4cb
Rafał <narudev1502@gmail.com>
2020-11-07 22:43:14 +0100

    Optimize imports and Add running multiple files

--- src/main/java/pl/naru/Run.java
+++ src/main/java/pl/naru/Run.java
@@ -3,36 +3,37 @@ package pl.naru;
 import pl.naru.workers.DataWorker;
 import pl.naru.workers.FileWorker;
 
-import java.io.*;
+import java.io.FileNotFoundException;
 import java.util.List;
 
 
 public class Run {
 
     public static void main(String[] args) {
-        FileWorker fileWorker = new FileWorker();
-        DataWorker dataWorker;
+        final FileWorker fileWorker = new FileWorker();
+
         if (args.length < 1) {
             System.out.println("Type in your file path");
             System.exit(0);
         }
-        try {
-
-            final List<String> lines = fileWorker.retrieveFileData(args[0]);
-            dataWorker = new DataWorker(lines);
-            if (lines == null || !dataWorker.isStatementValid()) {
-                System.out.println("invalid statement. Please make sure you have typed in at the end of your statement: \"apply\"<space><number>");
-                System.exit(0);
-            }
+        for (String n : args) {
             try {
-                System.out.println(dataWorker.executeStatement());
-            } catch (UnsupportedOperationException uoe) {
-                System.out.println(uoe.getMessage());
+
+                final List<String> lines = fileWorker.retrieveFileData(n);
+                final DataWorker dataWorker = new DataWorker(lines);
+                if (lines == null || !dataWorker.isStatementValid()) {
+                    System.out.println("invalid statement. Please make sure you have typed in at the end of your statement: \"apply\"<space><number>");
+                    System.exit(0);
+                }
+                try {
+                    System.out.println(dataWorker.executeStatement());
+                } catch (UnsupportedOperationException uoe) {
+                    System.out.println(uoe.getMessage());
+                }
+
+            } catch (FileNotFoundException ex) {
+                System.out.println("Unable to find a file in the given path");
             }
-
-        } catch (FileNotFoundException ex) {
-            System.out.println("Unable to find a file in the given path");
         }
-
     }
 }
--- src/main/java/pl/naru/workers/FileWorker.java
+++ src/main/java/pl/naru/workers/FileWorker.java
@@ -1,6 +1,10 @@
 package pl.naru.workers;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
